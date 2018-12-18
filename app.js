82a111f2b4a2de9f1bb04f7dcb2a95c7ec2bce99
esaniello <shaniel.samadhan.natin@gmail.com>
2018-12-18 16:44:50 -0300

    An instagram app that will query your ow user data

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const Instagram = require("node-instagram").default;
 
+//With the current instagram api you can only query the details of the
+//account who has the access token and the users you invite to your sandbox. :(
+
 const instagram = new Instagram({
     //get these 3 values by registering for an instagram developer account and then creating a new app.
     clientId: '...',
